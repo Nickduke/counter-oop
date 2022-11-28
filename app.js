3f4c48b0784b4d6cbfbd67a33b16c7eb336fa962
Nick <ntoukas.nick@gmail.com>
2022-11-27 19:43:51 -0500

    counter functions working

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-function Counter(element, value) {
-  console.log(element, value);
-}
-
-const firstCounter = new Counter(getElement('.first-counter'), 100);
-const secondCounter = new Counter(getElement('.second-counter'), 200);
-
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -14,3 +7,31 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector('.reset');
+  this.increaseBtn = element.querySelector('.increase');
+  this.decreaseBtn = element.querySelector('.decrease');
+  this.valueDOM = element.querySelector('.value');
+  this.valueDOM.textContent = this.value;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement('.first-counter'), 0);
+const secondCounter = new Counter(getElement('.second-counter'), 0);
+
+firstCounter.increase();
